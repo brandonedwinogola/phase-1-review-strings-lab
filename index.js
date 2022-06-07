d48c8bf4a30386ffcd6d74b4e9210f67b55aecf3
bran don <106029484+brandonedwinogola@users.noreply.github.com>
2022-06-07 15:28:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'bran don';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
